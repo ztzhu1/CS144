41897ed611e816361d4655e642f275d9b4522e07
ztzhu1 <zhuzitian@zju.edu.cn>
2022-11-16 21:31:48 +0800

    Pass all tests in lab2

--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -3,7 +3,7 @@
 
 #include "byte_stream.hh"
 #include "stream_reassembler.hh"
-#include "tcp_segment.hh"
+#include "tcp_helpers/tcp_segment.hh"
 #include "wrapping_integers.hh"
 
 #include <optional>
@@ -20,12 +20,15 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    bool _syn = false;
+    WrappingInt32 _isn;
+
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) ,_isn(0u) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
